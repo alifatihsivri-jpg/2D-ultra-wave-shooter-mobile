3a36f593c432034134de20c00e0f85cd3f2b7490
alifatihsivri-jpg <ali.fatih.sivri@gmail.com>
2026-03-16 23:55:13 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "wave-shooter-v1";
+const FILES = ["./savasoyunu76.html", "./manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
